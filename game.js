68ad3725040241833d173b7adfe11dd3f7200292
azizck <omarazi@sheridancollege.ca>
2019-09-02 13:39:53 -0400

    done

--- game.js
+++ game.js
@@ -1,3 +1,39 @@
 // Write all your code here
 // Replace the contents of this file with your own code
-console.log('Hello, world!');
+const choices = ["ROCK","SCISSOR","PAPER"];
+function computerPlay(){
+	return choices[Math.floor(Math.random()*choices.length)] ;
+}
+
+function playRound(playerSelection,computerSelection){
+	playerSelection = playerSelection.toUpperCase();
+	if( (playerSelection !== "ROCK") && (playerSelection !== "PAPER") && (playerSelection !== "SCISSOR") ){
+		return "INVALID INPUT";
+	}
+		computerWin=false;
+		if (playerSelection===computerSelection){
+			return "Tie !" 
+		}
+		else if(playerSelection == "ROCK" && computerSelection=="PAPER"){
+			computerWin=true;
+		} else if(playerSelection == "PAPER" && computerSelection=="SCISSOR"){
+			computerWin=true;
+		} else if(playerSelection == "SCISSOR" && computerSelection=="ROCK"){
+			computerWin=true;
+		}
+
+			if(computerWin)
+				return `Computer win ! because ${computerSelection} beats ${playerSelection}`;
+			else 
+				return `You Win because ${playerSelection} beats ${computerSelection}`;
+
+}
+
+	function game (){
+	for(let i=0; i<5; i++){
+	playerSelection=prompt("Enter your choice");
+	let result  = (playRound(playerSelection,computerPlay()));
+	alert(result);
+	}
+}
+game();
