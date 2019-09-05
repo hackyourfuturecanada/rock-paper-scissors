e482ab7cbd61b7021292307e8c373f43ee80543f
esyasar <elifsueyasar@gmail.com>
2019-09-05 00:41:20 -0400

    final project

--- game.js
+++ game.js
@@ -1,3 +1,34 @@
 // Write all your code here
 // Replace the contents of this file with your own code
-console.log('Hello, world!');
+const options = ['rock', 'paper', 'scissors'];
+
+function computerPlay() {
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  console.log('Player chose:' + playerSelection);
+console.log('Computer chose:' + computerSelection);
+
+if (playerSelection === computerSelection) {
+return 'You made the same choice! Try Again';
+} else if ((playerSelection === 'paper' && computerSelection === 'rock') ||
+   (playerSelection === 'rock' && computerSelection === 'scissors') ||
+   (playerSelection === 'scissors' && computerSelection === 'paper')) {
+       return 'You win!';
+   }
+  else{ return 'Computer win!';
+}
+}
+
+function game() {
+  let rounds= 0;
+while (rounds < 5) {
+const playerSelection = prompt('Choose one: rock, paper, scissors!');
+const result = playRound(playerSelection, computerPlay());
+console.log(result);
+rounds++;
+  }
+}
+
+game();
