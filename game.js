5313aa0b70a5c1bdbf6f4d2bf1ccd16fca453c12
aalitenaye <aalitenaye.sattari@gmail.com>
2019-09-04 21:59:58 -0400

    Game Submission

--- game.js
+++ game.js
@@ -1,3 +1,24 @@
 // Write all your code here
 // Replace the contents of this file with your own code
-console.log('Hello, world!');
+var array1 = ['Paper', 'Scissors', 'Rock']
+
+function computerPlay() {
+  const index = Math.floor(Math.random()*array1.length);
+  return index;
+}
+
+console.log(computerPlay());
+let result = computerPlay();
+console.log(result)
+
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+}
+
+const playerSelection = 'rock'
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
+
+function game() {
+  
+}
