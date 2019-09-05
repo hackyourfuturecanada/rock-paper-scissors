06739bd11801e2522534261de36c8b8d1c0afcac
hamzakoc <hamzakooc@gmail.com>
2019-09-04 20:46:55 -0400

    rock-paper-scissors homework submission

--- game.js
+++ game.js
@@ -1,3 +1,48 @@
-// Write all your code here
-// Replace the contents of this file with your own code
-console.log('Hello, world!');
+
+
+
+let choices = ["rock", "paper", "scissors"];
+
+function computerPlay(){
+	return choices[Math.floor(Math.random()*3)] ;
+}
+
+function playRound(playerSelection,computerSelection){
+	
+	if( playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors" ){
+		return "type a valid value";
+	}
+
+
+		
+    compWin=false;
+
+		if (playerSelection===computerSelection){
+			return "tie" 
+		}
+		else if(playerSelection == "rock" && computerSelection=="paper"){
+		    	compWin=true;
+		} else if(playerSelection == "paper" && computerSelection=="scissors"){
+			    compWin=true;
+		} else if(playerSelection == "scissors" && computerSelection=="rock"){
+			    compWin=true;
+		}
+
+
+			if(compWin)
+				return "You loose ! " + computerSelection + " beats " + playerSelection;
+			else 
+				return "You won!" + playerSelection + "beats" + computerSelection;
+
+}
+
+
+
+	function game (){
+	for(let i=0; i<5; i++){
+	const playerSelection=prompt("Type your choices");
+	let result  = (playRound(playerSelection,computerPlay()));
+	alert(result);
+	}
+}
+game(); 
