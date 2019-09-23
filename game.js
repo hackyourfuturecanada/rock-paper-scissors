d1be23bba054710b9bbbafd048636c8da5a54530
esyasar <elifsueyasar@gmail.com>
2019-09-23 02:14:19 -0400

    rps game part2

--- game.js
+++ game.js
@@ -1,34 +1,54 @@
-// Write all your code here
-// Replace the contents of this file with your own code
+
 const options = ['rock', 'paper', 'scissors'];
 
+document.getElementById('rock').addEventListener('click', function(e){
+    let round = playRound('rock', computerPlay());
+});
+document.getElementById('paper').addEventListener('click', function(e){
+    let round = playRound('paper', computerPlay());
+});
+document.getElementById('scissors').addEventListener('click', function(e){
+    let round = playRound('scissors', computerPlay());
+});
+
+//computer's choice
 function computerPlay() {
     return options[Math.floor(Math.random() * options.length)];
 }
 
+function addPointsToUser(){
+  points += 1;
+  document.getElementById("userScore")[0].innerHTML = points;
+}
+
+function addPointsToComp(){
+  points += 1;
+  document.getElementById("pcScore")[1].innerHTML = points;
+}
+
+
 function playRound(playerSelection, computerSelection) {
-  console.log('Player chose:' + playerSelection);
-console.log('Computer chose:' + computerSelection);
 
 if (playerSelection === computerSelection) {
-return 'You made the same choice! Try Again';
-} else if ((playerSelection === 'paper' && computerSelection === 'rock') ||
-   (playerSelection === 'rock' && computerSelection === 'scissors') ||
-   (playerSelection === 'scissors' && computerSelection === 'paper')) {
-       return 'You win!';
-   }
-  else{ return 'Computer win!';
-}
-}
+  document.getElementById("p").innerHTML = "You made the same choice! Try Again";
 
-function game() {
-  let rounds= 0;
-while (rounds < 5) {
-const playerSelection = prompt('Choose one: rock, paper, scissors!');
-const result = playRound(playerSelection, computerPlay());
-console.log(result);
-rounds++;
+  } else if((playerSelection === 'paper' && computerSelection === 'rock') ||
+       (playerSelection === 'rock' && computerSelection === 'scissors') ||
+       (playerSelection === 'scissors' && computerSelection === 'paper')) {
+            document.getElementById("p").innerHTML = "You win! You chose:" + playerSelection;
+            addPointsToUser();
+          }
+          else{  document.getElementById("p").innerHTML = "Computer win!";
+          addPointsToComp();
   }
 }
 
-game();
+ /* function game() {
+  let rounds= 0;
+while (rounds < 5) {
+const result = playRound(playerSelection, computerPlay());
+const whoisthewinner = prompt(result);
+rounds++;
+  }
+}
+game(); */
